11528bcf7d60c88e51191ad6456b3eff44dc1c94
pholmes10 <pholmes10@gmail.com>
2018-11-13 22:38:56 -0600

    added functionality for game with just blue gem

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,58 @@
-var targetNumber = 50;
-var counter = 0;
-var numberOptions = [1, 2, 3, 4];
+var targetNumber = Math.floor(Math.random() * (102)) + 19;
+var blue = Math.floor(Math.random() * (11)) + 1;
+var green = Math.floor(Math.random() * (11)) + 1;
+var red = Math.floor(Math.random() * (11)) + 1;
+var yellow = Math.floor(Math.random() * (11)) + 1;
+console.log(targetNumber);
+console.log(blue);
+console.log(green);
+console.log(red);
+console.log(yellow);
 
-for (var i = 0; i < numberOptions.length; i++) {
-    var imageCrystal = $("<img>");
-    imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "assets/images/blue.png");
-    $(".crystals").append(imageCrystal);
+$("#targetScore").text(targetNumber);
+
+var wins = 0;
+var losses = 0;
+var counter = 0;
+
+$("#wins").text(wins);
+$("#losses").text(losses);
+
+function start() {
+    counter = 0;
+    $("#currentScore").text(counter);
+    targetNumber = Math.floor(Math.random() * (102)) + 19;
+    $("#targetScore").text(targetNumber);
+    blue = Math.floor(Math.random() * (11)) + 1;
+    green = Math.floor(Math.random() * (11)) + 1;
+    red = Math.floor(Math.random() * (11)) + 1;
+    yellow = Math.floor(Math.random() * (11)) + 1;
 };
-$(".crystal-image").on("click", function () {
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    counter += crystalValue;
-})
+
+function winner() {
+    wins++;
+    $("#wins").text(wins);
+    start();
+}
+
+function loser() {
+    losses++;
+    $("#losses").text(losses);
+    start();
+}
+
+var blueCrystalImage = $("<img>");
+blueCrystalImage.attr("src", "assets/images/blue.png");
+blueCrystalImage.attr("data-bluevalue", blue);
+$("#blueCrystal").append(blueCrystalImage);
+
+$("#blueCrystal").on("click", function () {
+    counter = counter + blue;
+    console.log(counter);
+    $("#currentScore").text(counter);
+    if (counter === targetNumber) {
+        winner();
+    } else if (counter > targetNumber) {
+        loser();
+    }
+});
